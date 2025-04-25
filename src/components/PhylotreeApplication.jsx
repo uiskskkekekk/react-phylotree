bc7790ad8f28c4e6185b1f6ccf14e68c81aebc26
uiskskkekekk <angus.lu0611@gmail.com>
2025-04-25 22:52:03 +0800

    export img

--- src/components/PhylotreeApplication.jsx
+++ src/components/PhylotreeApplication.jsx
@@ -625,6 +625,151 @@ class PhylotreeApplication extends Component {
     }
   };
 
+  // exportTreeAsImage = () => {
+  //   const svgElement = document.querySelector(".tree_container svg");
+  //   if (!svgElement) {
+  //     alert("無法找到 SVG 元素");
+  //     return;
+  //   }
+
+  //   // 創建 SVG 的副本，以便我們可以修改它而不影響原始 SVG
+  //   const svgClone = svgElement.cloneNode(true);
+
+  //   // 設置白色背景
+  //   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  //   rect.setAttribute("width", "100%");
+  //   rect.setAttribute("height", "100%");
+  //   rect.setAttribute("fill", "white");
+  //   svgClone.insertBefore(rect, svgClone.firstChild);
+
+  //   // 將 SVG 轉換為字符串
+  //   const svgData = new XMLSerializer().serializeToString(svgClone);
+
+  //   // 創建 Blob
+  //   const svgBlob = new Blob([svgData], {
+  //     type: "image/svg+xml;charset=utf-8",
+  //   });
+  //   const url = URL.createObjectURL(svgBlob);
+
+  //   // 創建 Image 對象
+  //   const img = new Image();
+
+  //   img.onload = () => {
+  //     // 創建 canvas
+  //     const canvas = document.createElement("canvas");
+  //     canvas.width = svgElement.width.baseVal.value;
+  //     canvas.height = svgElement.height.baseVal.value;
+
+  //     // 在 canvas 上繪製圖像
+  //     const ctx = canvas.getContext("2d");
+  //     ctx.fillStyle = "white";
+  //     ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //     ctx.drawImage(img, 0, 0);
+
+  //     // 轉換為圖片 URL
+  //     try {
+  //       const imgURL = canvas.toDataURL("image/png");
+
+  //       // 創建下載鏈接
+  //       const downloadLink = document.createElement("a");
+  //       downloadLink.download = "phylotree.png";
+  //       downloadLink.href = imgURL;
+  //       downloadLink.click();
+
+  //       // 清理
+  //       URL.revokeObjectURL(url);
+  //     } catch (e) {
+  //       console.error("導出圖片時出錯：", e);
+  //       alert(
+  //         "導出圖片失敗，可能是由於瀏覽器安全限制。請嘗試較小的樹或檢查控制台獲取更多信息。"
+  //       );
+  //     }
+  //   };
+
+  //   img.onerror = () => {
+  //     console.error("無法從 SVG 創建圖像");
+  //     URL.revokeObjectURL(url);
+  //     alert("無法創建圖像。請檢查控制台獲取更多信息。");
+  //   };
+
+  //   // 設置圖像源並開始加載
+  //   img.src = url;
+  // };
+
+  exportTreeAsImage = () => {
+    const scaleFactor = 5;
+
+    const svgElement = document.querySelector(".tree_container svg");
+    if (!svgElement) {
+      alert("無法找到 SVG 元素");
+      return;
+    }
+
+    // 創建 SVG 的副本
+    const svgClone = svgElement.cloneNode(true);
+
+    // 設置白色背景
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute("width", "100%");
+    rect.setAttribute("height", "100%");
+    rect.setAttribute("fill", "white");
+    svgClone.insertBefore(rect, svgClone.firstChild);
+
+    // 將 SVG 轉換為字符串
+    const svgData = new XMLSerializer().serializeToString(svgClone);
+
+    // 創建 Blob
+    const svgBlob = new Blob([svgData], {
+      type: "image/svg+xml;charset=utf-8",
+    });
+    const url = URL.createObjectURL(svgBlob);
+
+    // 創建 Image 對象
+    const img = new Image();
+
+    img.onload = () => {
+      // 創建 canvas，使用縮放因子提高解析度
+      const canvas = document.createElement("canvas");
+      canvas.width = svgElement.width.baseVal.value * scaleFactor;
+      canvas.height = svgElement.height.baseVal.value * scaleFactor;
+
+      // 在 canvas 上繪製圖像
+      const ctx = canvas.getContext("2d");
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.scale(scaleFactor, scaleFactor);
+      ctx.drawImage(img, 0, 0);
+
+      // 轉換為圖片 URL
+      try {
+        const imgURL = canvas.toDataURL("image/png");
+
+        // 創建下載鏈接
+        const downloadLink = document.createElement("a");
+        downloadLink.download = "phylotree.png";
+        downloadLink.href = imgURL;
+        downloadLink.click();
+
+        // 清理
+        URL.revokeObjectURL(url);
+      } catch (e) {
+        console.error("導出圖片時出錯：", e);
+        alert(
+          "導出圖片失敗，可能是由於瀏覽器安全限制。請嘗試較小的樹或降低解析度。"
+        );
+      }
+    };
+
+    img.onerror = () => {
+      console.error("無法從 SVG 創建圖像");
+      URL.revokeObjectURL(url);
+      alert("無法創建圖像。請檢查控制台獲取更多信息。");
+    };
+
+    // 設置圖像源並開始加載
+    img.src = url;
+  };
+
   render() {
     const { padding } = this.props;
     const { width, height, clickedBranch, contextMenu, collapsedNodes } =
@@ -652,7 +797,8 @@ class PhylotreeApplication extends Component {
                 onChange={this.handleFileChange}
                 style={{ marginTop: "20px" }}
               />
-              <button onClick={this.exportModifiedNewick}>Export</button>
+              <button onClick={this.exportModifiedNewick}>Export Newick</button>
+              <button onClick={this.exportTreeAsImage}>Export Image</button>
             </div>
             <div className="button-group-container">
               <ButtonGroup>
